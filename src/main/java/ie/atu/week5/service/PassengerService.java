b91bb4ad8a00898db0c6b4d8644833b1ca682e11
LANCECRUZ-STUDENT <g00428876@atu.ie>
2025-10-15 13:43:40 +0100

    Added service Passenger code

--- src/main/java/ie/atu/week5/service/PassengerService.java
+++ src/main/java/ie/atu/week5/service/PassengerService.java
@@ -1,4 +1,33 @@
 package ie.atu.week5.service;
 
+import ie.atu.week5.model.Passenger;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PassengerService {
+    private final List<Passenger> store = new ArrayList<>();
+
+    public List<Passenger> findAll() {
+        return new ArrayList<>(store);
+    }
+
+    public Optional<Passenger> findbyId(String id) {
+        for (Passenger p : store) {
+            if (p.getPassengerId().equals(id))
+                return Optional.of(p);
+        }
+        return Optional.empty();
+    }
+
+    public Passenger create(Passenger p) {
+        if (findbyId(p.getPassengerId()).isPresent()) {
+            throw new IllegalArgumentException("passengerId already exists");
+        }
+        store.add(p);
+        return p;
+    }
 }
